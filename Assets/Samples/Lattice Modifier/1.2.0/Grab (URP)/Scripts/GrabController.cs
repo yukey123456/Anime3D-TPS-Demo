2ef53bb9349bc64dcf8eb06e4a479a1841bf0809
yukey123456 <97617312+yukey123456@users.noreply.github.com>
2025-02-17 21:17:55 +0700

    feat: setup lattice for boob

--- Assets/Samples/Lattice Modifier/1.2.0/Grab (URP)/Scripts/GrabController.cs
+++ Assets/Samples/Lattice Modifier/1.2.0/Grab (URP)/Scripts/GrabController.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+	using UnityEngine;
 
 namespace Lattice.Samples
 {
@@ -50,25 +50,25 @@ namespace Lattice.Samples
 
 		private void Update()
 		{
-			if (Input.GetMouseButtonDown(0))
-			{
-				StartGrab();
-			}
+			//if (Input.GetMouseButtonDown(0))
+			//{
+			//	StartGrab();
+			//}
 
-			if (Input.GetMouseButtonUp(0))
-			{
-				ReleaseGrab();
-			}
+			//if (Input.GetMouseButtonUp(0))
+			//{
+			//	ReleaseGrab();
+			//}
 
 			for (int i = 0; i < _lattices.Length; i++)
 			{
 				UpdateLattice(i);
 			}
 
-			if (_grabbing)
-			{
-				UpdateGrab();
-			}
+			//if (_grabbing)
+			//{
+			//	UpdateGrab();
+			//}
 		}
 
 		private void UpdateLattice(int index)
@@ -86,72 +86,72 @@ namespace Lattice.Samples
 			lattice.SetHandleWorldPosition(Handle, state.Position);
 		}
 
-		private void StartGrab()
-		{
-			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+		//private void StartGrab()
+		//{
+		//	Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
 
-			// Check mouse intersection with head
-			if (_headCollider.Raycast(ray, out RaycastHit hitInfo, 100f))
-			{
-				Vector3 position = hitInfo.point;
-				Lattice lattice = _lattices[_latticeIndex];
-				ref State state = ref _states[_latticeIndex];
+		//	// Check mouse intersection with head
+		//	if (_headCollider.Raycast(ray, out RaycastHit hitInfo, 100f))
+		//	{
+		//		Vector3 position = hitInfo.point;
+		//		Lattice lattice = _lattices[_latticeIndex];
+		//		ref State state = ref _states[_latticeIndex];
 				
-				// Start grabbing, set initial position to intersection,
-				// and create camera facing plane at intersection.
-				_grabPosition = position;
-				_grabPlane = new Plane(-_camera.transform.forward, _grabPosition);
-				_grabbing = true;
+		//		// Start grabbing, set initial position to intersection,
+		//		// and create camera facing plane at intersection.
+		//		_grabPosition = position;
+		//		_grabPlane = new Plane(-_camera.transform.forward, _grabPosition);
+		//		_grabbing = true;
 
-				// Move lattice so that handle is at intersection
-				Vector3 offset = lattice.transform.TransformVector(-lattice.GetHandleBasePosition(Handle));
-				lattice.transform.position = _grabPosition + offset;
-				state = new State() { Position = _grabPosition };
+		//		// Move lattice so that handle is at intersection
+		//		Vector3 offset = lattice.transform.TransformVector(-lattice.GetHandleBasePosition(Handle));
+		//		lattice.transform.position = _grabPosition + offset;
+		//		state = new State() { Position = _grabPosition };
 
-				// Start next pose
-				_headController.IncrementPose();
-			}
-		}
+		//		// Start next pose
+		//		_headController.IncrementPose();
+		//	}
+		//}
 
-		private void UpdateGrab()
-		{
-			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+		//private void UpdateGrab()
+		//{
+		//	Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
 
-			// Check mouse intersection with camera facing plane
-			if (_grabPlane.Raycast(ray, out float dist))
-			{
-				Vector3 position = ray.GetPoint(dist);
-				Lattice lattice = _lattices[_latticeIndex];
-				ref State state = ref _states[_latticeIndex];
+		//	// Check mouse intersection with camera facing plane
+		//	if (_grabPlane.Raycast(ray, out float dist))
+		//	{
+		//		Vector3 position = ray.GetPoint(dist);
+		//		Lattice lattice = _lattices[_latticeIndex];
+		//		ref State state = ref _states[_latticeIndex];
 
-				// Estimate mouse velocity
-				Vector3 velocity = Vector3.Lerp(
-					(position - state.Position) / Time.deltaTime,
-					state.Velocity,
-					0.8f
-				);
+		//		// Estimate mouse velocity
+		//		Vector3 velocity = Vector3.Lerp(
+		//			(position - state.Position) / Time.deltaTime,
+		//			state.Velocity,
+		//			0.8f
+		//		);
 
-				// Update lattice handle to match mouse position and velocity
-				state = new State()
-				{
-					Position = position,
-					Velocity = velocity
-				};
+		//		// Update lattice handle to match mouse position and velocity
+		//		state = new State()
+		//		{
+		//			Position = position,
+		//			Velocity = velocity
+		//		};
 
-				lattice.SetHandleWorldPosition(Handle, position);
+		//		lattice.SetHandleWorldPosition(Handle, position);
 
-				// Increase pose strength by distance from original location
-				float grabDistance = Vector3.Distance(position, _grabPosition);
-				_headController.SetPoseStrength(Mathf.Clamp01(3f * grabDistance));
-			}
-		}
+		//		// Increase pose strength by distance from original location
+		//		float grabDistance = Vector3.Distance(position, _grabPosition);
+		//		_headController.SetPoseStrength(Mathf.Clamp01(3f * grabDistance));
+		//	}
+		//}
 
-		private void ReleaseGrab()
-		{
-			// Stop grabbing and use next lattice for next grab
-			_grabbing = false;
-			_latticeIndex = (_latticeIndex + 1) % _lattices.Length;
-			_headController.SetPoseStrength(1f);
-		}
+		//private void ReleaseGrab()
+		//{
+		//	// Stop grabbing and use next lattice for next grab
+		//	_grabbing = false;
+		//	_latticeIndex = (_latticeIndex + 1) % _lattices.Length;
+		//	_headController.SetPoseStrength(1f);
+		//}
 	}
 }
